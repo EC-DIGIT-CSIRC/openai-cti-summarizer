992edf39f51e98088438630d0c1e3405dd09af66
aaronkaplan <aaron@lo-res.org>
2023-03-21 11:09:42 +0100

    type was wrong

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str = None # To check if supplied
+    OPENAI_API_KEY: str = "" # To check if supplied
     DRY_RUN: bool = True
 
     class Config:
