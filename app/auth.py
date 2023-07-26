5dc94462a8e6377ae33337e9b3ce3792e03b8659
aaronkaplan <aaron@lo-res.org>
2023-07-26 23:07:55 +0200

    fix problems which appeared due to a pydantic upgrade. Fixes #91

--- app/auth.py
+++ app/auth.py
@@ -10,6 +10,7 @@ fake_users = {
     "digit": "united we stand, divided we fall!",   # XXX FIXME: currently this is intentionally simple and in the code. We will replace this with proper authentication. It's just against bots misusing the service automatically.
 }
 
+
 # dependency to check if the credentials are valid
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     username = credentials.username
