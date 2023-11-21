2a28042caca70f4d5d4c6fdde5a3fe87cb83a9cb
Aaron Kaplan <aaron@lo-res.org>
2023-11-21 13:29:25 +0000

    upgrade top the new openai client API. fix some bugs

--- app/main.py
+++ app/main.py
@@ -1,39 +1,65 @@
 import os
+import sys
 
 import uvicorn
-from fastapi import FastAPI, Request, Form, HTTPException, Depends
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request, Form, Depends
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.base import BaseHTTPMiddleware
 import markdown
 
 from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv, find_dotenv
 
 from summarizer import Summarizer
 # from settings import Settings
 from auth import get_current_username
+from distutils.util import strtobool
 
+
+DEBUG = True
 
 # first get the env parametting
-from dotenv import load_dotenv, find_dotenv
 if not load_dotenv(find_dotenv(), verbose=True, override=False):     # read local .env file
     print("Could not find .env file! Assuming ENV vars work")
 
-app = FastAPI(version=os.environ['VERSION'])
+try:
+    VERSION = open('../VERSION.txt', encoding='utf-8').readline().rstrip('\n')
+except Exception as e:
+    print("could not find VERSION.txt, bailing out.")
+    sys.exit(-1)
+
+
+app = FastAPI(version=VERSION)
 templates = Jinja2Templates(directory="/templates")
 app.mount("/static", StaticFiles(directory="/static"), name="static")
+GO_AZURE = False    # default
+DRY_RUN = bool(strtobool(os.getenv('DRY_RUN', 'false')))
 
 
 # First detect if we should invoke OpenAI via MS Azure or directly
 try:
-    go_azure = os.environ['USE_MS_AZURE']
+    GO_AZURE = bool(strtobool(os.getenv('USE_MS_AZURE', 'false')))
 except Exception as e:
-    go_azure = False
+    print(
+        f"Could not read 'USE_MS_AZURE' env var. Reason: '{str(e)}'. Reverting to false.")
+    GO_AZURE = False
 
 
-summarizer = Summarizer(go_azure=go_azure, model='gpt-3.5-turbo-16k', max_tokens=500)
+class HTTPSRedirectMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if 'X-Forwarded-Proto' in request.headers and request.headers['X-Forwarded-Proto'] == 'https':
+            request.scope['scheme'] = 'https'
+        response = await call_next(request)
+        return response
+
+
+app.add_middleware(HTTPSRedirectMiddleware)
+
+summarizer = Summarizer(go_azure=GO_AZURE, model='gpt-4-1106-preview', max_tokens=8192)
 
 
 async def fetch_text_from_url(url: str) -> str:
@@ -44,7 +70,7 @@ async def fetch_text_from_url(url: str) -> str:
     if not all([parsed_url.scheme, parsed_url.netloc]):
         raise ValueError("Invalid URL")
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=5)
     soup = BeautifulSoup(response.text, 'html.parser')
     text = soup.get_text()
     return text
@@ -52,19 +78,23 @@ async def fetch_text_from_url(url: str) -> str:
 
 @app.get("/", response_class=HTMLResponse)
 def get_index(request: Request, username: str = Depends(get_current_username)):
-    return templates.TemplateResponse("index.html", {"request": request, "system_prompt": os.environ['SYSTEM_PROMPT']})
+    """Return the default page."""
+    return templates.TemplateResponse("index.html", {"request": request, "system_prompt": os.environ['SYSTEM_PROMPT'], "username": username})
 
 
 @app.post("/", response_class=HTMLResponse)
-async def index(request: Request, text: str = Form(None), url: str = Form(None), system_prompt: str = Form(None), model: str = Form('gpt-4'), word_count: int = Form(100), username: str = Depends(get_current_username)):
+async def index(request: Request, text: str = Form(None), url: str = Form(None),
+                system_prompt: str = Form(None), model: str = Form('gpt-4'), token_count: int = Form(100),
+                username: str = Depends(get_current_username)):
+    """HTTP POST method for the default page. This gets called when the user already HTTP POSTs a text which should be summarized."""
 
     if not url and not text:
         error = "Expected either url field or text field. Please specify one at least."
         result = None
-        return templates.TemplateResponse("index.html", {"request": request, "text": text, "system_prompt": system_prompt, "result": error, "success": False}, status_code=400)
+        return templates.TemplateResponse("index.html", {"request": request, "text": text, "system_prompt": system_prompt, "result": error, "success": False, "username": username}, status_code=400)
 
     summarizer.model = model
-    summarizer.max_tokens = word_count
+    summarizer.max_tokens = token_count
 
     if url:
         # go and fetch it
@@ -73,17 +103,25 @@ async def index(request: Request, text: str = Form(None), url: str = Form(None),
         except Exception as ex:
             return templates.TemplateResponse("index.html", {"request": request, "text": url, "system_prompt": system_prompt, "result": f"Could not fetch URL. Reason {str(ex)}", "success": False}, status_code=400)
 
-    if os.environ['DRY_RUN'] and int(os.environ['DRY_RUN']) >= 1:
+    if DRY_RUN:
         result = "This is a sample response, we are in dry-run mode. We don't want to waste money for querying the API."
         error = None
     else:
         result, error = summarizer.summarize(text, system_prompt)
 
     if error:
-        return templates.TemplateResponse("index.html", {"request": request, "text": text, "system_prompt": system_prompt, "result": error, "success": False}, status_code=400)
+        return templates.TemplateResponse("index.html", {"request": request, "text": text, "system_prompt": system_prompt, "result": error, "success": False, "username": username}, status_code=400)
 
     result = markdown.markdown(result)
-    return templates.TemplateResponse("index.html", {"request": request, "text": text, "system_prompt": system_prompt, "result": result, "success": True, "model": model, "word_count": word_count})
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "text": text,
+        "system_prompt": system_prompt,
+        "result": result,
+        "success": True,
+        "model": model,
+        "username": username,
+        "token_count": token_count})
 
 
 if __name__ == "__main__":
--- app/summarizer.py
+++ app/summarizer.py
@@ -2,7 +2,7 @@ import os
 import pprint
 
 import openai
-import openai.error
+from openai import AzureOpenAI
 from typing import Tuple
 
 # first get the env parametting
@@ -10,8 +10,13 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())      # read local .env file
 
 
+# client = AzureOpenAI(api_version=os.environ['OPENAI_API_VERSION'], api_key=os.environ['OPENAI_API_KEY'])
+client = openai.OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+
+
 class Summarizer:
     """Wrapper to summarize texts via OpenAI or MS Azure's OpenAI."""
+
     def __init__(self, model: str, max_tokens: int, system_prompt: str = "", go_azure: bool = False):
         if system_prompt:
             self.system_prompt = system_prompt
@@ -22,24 +27,23 @@ class Summarizer:
         self.go_azure = go_azure
         if self.go_azure:
             # openai.api_type = "azure"
-            openai.api_type = os.environ['OPENAI_API_TYPE']
-            openai.api_base = os.environ['OPENAI_API_BASE']             # Your Azure OpenAI resource's endpoint value.
-            openai.api_version = os.environ['OPENAI_API_VERSION']       # "2023-05-15"
-            openai.api_key = os.environ['OPENAI_API_KEY']
+
+            # TODO: The 'openai.api_base' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(api_base=os.environ['OPENAI_API_BASE'])'
+            # openai.api_base = os.environ['OPENAI_API_BASE']             # Your Azure OpenAI resource's endpoint value.
+            # "2023-05-15"
+
             """
             openai.api_type = os.environ['OPENAI_API_TYPE']
             openai.api_base = os.environ['OPENAI_API_BASE']             # "https://devmartiopenai.openai.azure.com/"
             openai.api_version = os.environ['OPENAI_API_VERSION']       # "2023-05-15"
             """
-        openai.api_key = os.environ['OPENAI_API_KEY']
-        pprint.pprint({
-            "api_type": openai.api_type,
-            "api_version": openai.api_version,
-            "api_base": openai.api_base,
-            "api_key": openai.api_key,
-            "engine": os.environ['ENGINE']
+            pprint.pprint({
+                "api_type": openai.api_type,
+                "api_version": openai.api_version,
+                "api_base": openai.api_base,
+                "api_key": openai.api_key,
+                "engine": os.environ['ENGINE']
             })
-
 
     def summarize(self, text: str, system_prompt: str = "") -> Tuple[str, str]:
         """Send <text> to openAI and get a summary back.
@@ -52,36 +56,37 @@ class Summarizer:
             {"role": "user", "content": text}
         ]
 
-        # XXX FIXME: work with chunks
-
         try:
             if self.go_azure:
                 print("Using MS AZURE!")
-                response = openai.ChatCompletion.create(
-                    engine=os.environ['ENGINE'],
-                    messages=messages,
-                    temperature=0.7,
-                    top_p=0.95,
-                    stop=None,
-                    max_tokens=self.max_tokens,     # TODO: Make sure this actually means, summarize in X token
-                    n=1,
-                )
+                response = client.chat.completions.create(model=os.environ['ENGINE'],
+                                                          messages=messages,
+                                                          temperature=0.7,
+                                                          top_p=0.95,
+                                                          stop=None,
+                                                          # TODO: Make sure this actually means, summarize in X token
+                                                          max_tokens=self.max_tokens,
+                                                          n=1)
             else:       # go directly via OpenAI's API
                 print("Using OpenAI directly!")
-                response = openai.ChatCompletion.create(
-                    model=self.model,
-                    messages=messages,
-                    temperature=0.7,
-                    top_p=0.95,
-                    stop=None,
-                    max_tokens=self.max_tokens,
-                    n=1,
-                )
+                response = client.chat.completions.create(model=self.model,
+                                                          messages=messages,
+                                                          temperature=0.7,
+                                                          top_p=0.95,
+                                                          stop=None,
+                                                          max_tokens=self.max_tokens,
+                                                          n=1)
             result = response.choices[0].message.content
             error = None            # Or move the error handling back to main.py, not sure
-        except openai.error.OpenAIError as e:
+        except openai.APIConnectionError as e:
             result = None
-            error = f"OpenAI API returned an API Error: {str(e)}"
+            error = f"The server could not be reached. Reason {e.__cause__}"
+        except openai.RateLimitError as e:
+            result = None
+            error = f"A 429 status code was received; we should back off a bit. {str(e)}"
+        except openai.APIStatusError as e:
+            result = None
+            error = f"Another non-200-range status code was received. Status code: {e.status_code}. Response: {e.response}"
         except Exception as e:
             result = None
             error = f"Unknown error! Error = '{str(e)}'"
