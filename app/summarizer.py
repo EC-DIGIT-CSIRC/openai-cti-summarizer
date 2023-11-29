f1d284879852f0148994b93edfd2af96487bbf9f
Aaron Kaplan <aaron@lo-res.org>
2023-11-29 11:03:04 +0000

    fix it for the new openai client lib. Use GPT-4-turbo

--- app/summarizer.py
+++ app/summarizer.py
@@ -10,12 +10,10 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())      # read local .env file
 
 
-# client = AzureOpenAI(api_version=os.environ['OPENAI_API_VERSION'], api_key=os.environ['OPENAI_API_KEY'])
-client = openai.OpenAI(api_key=os.environ['OPENAI_API_KEY'])
-
-
 class Summarizer:
     """Wrapper to summarize texts via OpenAI or MS Azure's OpenAI."""
+
+    client: openai._base_client.BaseClient
 
     def __init__(self, model: str, max_tokens: int, system_prompt: str = "", go_azure: bool = False):
         if system_prompt:
@@ -26,7 +24,20 @@ class Summarizer:
         self.max_tokens = max_tokens
         self.go_azure = go_azure
         if self.go_azure:
-            # openai.api_type = "azure"
+            api_version = os.environ['OPENAI_API_VERSION']
+            azure_endpoint = os.environ['OPENAI_API_BASE']
+            azure_deployment = os.environ['ENGINE']
+            api_key = os.environ['AZURE_OPENAI_API_KEY']
+            print(f"""
+                {api_version=},
+                {azure_endpoint=},
+                {azure_deployment=},
+                {api_key=}
+            """)
+            self.client = AzureOpenAI(api_version=os.environ['OPENAI_API_VERSION'],
+                                      azure_endpoint=os.environ['OPENAI_API_BASE'],
+                                      azure_deployment=os.environ['ENGINE'],
+                                      api_key=os.environ['AZURE_OPENAI_API_KEY'])
 
             # TODO: The 'openai.api_base' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(api_base=os.environ['OPENAI_API_BASE'])'
             # openai.api_base = os.environ['OPENAI_API_BASE']             # Your Azure OpenAI resource's endpoint value.
@@ -37,13 +48,9 @@ class Summarizer:
             openai.api_base = os.environ['OPENAI_API_BASE']             # "https://devmartiopenai.openai.azure.com/"
             openai.api_version = os.environ['OPENAI_API_VERSION']       # "2023-05-15"
             """
-            pprint.pprint({
-                "api_type": openai.api_type,
-                "api_version": openai.api_version,
-                "api_base": openai.api_base,
-                "api_key": openai.api_key,
-                "engine": os.environ['ENGINE']
-            })
+            print(f"Using Azure client {self.client._version}")
+        else:
+            self.client = openai.OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
     def summarize(self, text: str, system_prompt: str = "") -> Tuple[str, str]:
         """Send <text> to openAI and get a summary back.
@@ -59,23 +66,26 @@ class Summarizer:
         try:
             if self.go_azure:
                 print("Using MS AZURE!")
-                response = client.chat.completions.create(model=os.environ['ENGINE'],
-                                                          messages=messages,
-                                                          temperature=0.7,
-                                                          top_p=0.95,
-                                                          stop=None,
-                                                          # TODO: Make sure this actually means, summarize in X token
-                                                          max_tokens=self.max_tokens,
-                                                          n=1)
+                response = self.client.chat.completions.create(model=os.environ['ENGINE'],
+                                                               messages=messages,
+                                                               temperature=0.7,
+                                                               top_p=0.95,
+                                                               stop=None,
+                                                               max_tokens=self.max_tokens,
+                                                               n=1)
             else:       # go directly via OpenAI's API
                 print("Using OpenAI directly!")
-                response = client.chat.completions.create(model=self.model,
-                                                          messages=messages,
-                                                          temperature=0.7,
-                                                          top_p=0.95,
-                                                          stop=None,
-                                                          max_tokens=self.max_tokens,
-                                                          n=1)
+                response = self.client.chat.completions.create(model=self.model,
+                                                               messages=messages,
+                                                               temperature=0.7,
+                                                               top_p=0.95,
+                                                               stop=None,
+                                                               max_tokens=self.max_tokens,
+                                                               n=1)
+                print(f"Response (OpenAI): {response}")
+                print(80*"=")
+                print(f"response (Openai).choices[0].text: {response.choices[0].message}")
+            print(response.model_dump_json(indent=2))
             result = response.choices[0].message.content
             error = None            # Or move the error handling back to main.py, not sure
         except openai.APIConnectionError as e:
@@ -86,7 +96,7 @@ class Summarizer:
             error = f"A 429 status code was received; we should back off a bit. {str(e)}"
         except openai.APIStatusError as e:
             result = None
-            error = f"Another non-200-range status code was received. Status code: {e.status_code}. Response: {e.response}"
+            error = f"Another non-200-range status code was received. Status code: {e.status_code}. \n\nResponse: {e.message}"
         except Exception as e:
             result = None
             error = f"Unknown error! Error = '{str(e)}'"
